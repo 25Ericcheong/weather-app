71863cb2c7b0787da991392813663f0e762a8d39
25Ericcheong <ericcheongkaikit@gmail.com>
2022-02-07 21:48:30 +1000

    got rid of files not being used

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import "./App.css";
 
+function formatName(user) {
+  return `This is ${user.firstName} ${user.lastName}'s first react project!`;
+}
+
 function App() {
-  const name = "Weather App";
-  const element = <h1>Personal {name}</h1>;
+  const user = {
+    firstName: "Eric",
+    lastName: "Cheong",
+  };
+
+  const element = <h1>{formatName(user)}</h1>;
 
   return <div className="App">{element}</div>;
 }
