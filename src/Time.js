b4f2a3885d7a67476ec99eb754521501a3b9dc0b
25Ericcheong <ericcheongkaikit@gmail.com>
2022-02-08 09:02:03 +1000

    utilizing class component

--- src/Time.js
+++ src/Time.js
@@ -1,13 +1,25 @@
 import "./App.css";
+import React from "react";
 
-function Time() {
-  const element = (
-    <div>
-      <h2>It is {new Date().toLocaleTimeString()}</h2>
-    </div>
-  );
-
-  return <div className="App">{element}</div>;
+class Time extends React.Component {
+  render() {
+    return (
+      <section className="App">
+        <h2>12:30pm</h2>
+      </section>
+    );
+  }
 }
+
+// function Time() {
+
+//   const element = (
+//     <div>
+//       <h2>It is {currentTime.date.toLocaleTimeString()}</h2>
+//     </div>
+//   );
+
+//   return <div className="App">{element}</div>;
+// }
 
 export default Time;
